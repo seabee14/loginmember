5acb5fe12be1932da3a6b0a61039019770290990
Glitch (carpal-towering-porpoise) <none>
2021-09-10 12:13:37 +0000

    👮🐯 Checkpoint ./controllers/dashboard.js:35422279/289 ./models/station-store.json:35422279/1011

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -2,14 +2,16 @@
 
 const logger = require("../utils/logger");
 const stationStore = require("../models/station-store.js");
+const accounts = require ('./accounts.js');
 const uuid = require('uuid');
 
 const dashboard = {
   index(request, response) {
     logger.info("dashboard rendering");
+    const loggedInUser = accounts.getCurrentUser(request);
     const viewData = {
       title: "Weathertop Dashboard",
-      stations: stationStore.getAllStations(),
+      stations: stationStore.getUserStations(loggedInUser.id),
     };
     logger.info("about to render", stationStore.getAllStations());
     response.render("dashboard", viewData);
@@ -22,13 +24,16 @@ const dashboard = {
   },
   
   addStation(request, response) {
+    const loggedInUser = accounts.getCurrentUser(request);
     const newStation = {
       id: uuid.v1(),
+      userid: loggedInUser.id,
       title: request.body.title,
       longitude: request.body.title,
       lattitude: request.body.title,
       readings: [],
     };
+    logger.debug('Creating a new Station', newStation);
     stationStore.addStation(newStation);
     response.redirect('/dashboard');
   },
